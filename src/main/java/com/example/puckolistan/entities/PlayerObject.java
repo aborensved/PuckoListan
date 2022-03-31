03e01f0eb02fa05fb61a29bba798bf61a2be3d37
aborensved <92864523+aborensved@users.noreply.github.com>
2022-03-31 09:58:14 +0200

    player object

--- src/main/java/com/example/puckolistan/entities/PlayerObject.java
+++ src/main/java/com/example/puckolistan/entities/PlayerObject.java
@@ -1,4 +1,104 @@
 package com.example.puckolistan.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class PlayerObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private int age;
+
+    @Column
+    private int height;
+
+    @Column
+    private int weight;
+
+    @Column
+    private String position;
+
+    @Column
+    private String info;
+
+    public PlayerObject(String firstName, String lastName, int age, int height, int weight, String position, String info) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.position = position;
+        this.info = info;
+    }
+
+    public PlayerObject(){};
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getInfo() {
+        return info;
+    }
 }
